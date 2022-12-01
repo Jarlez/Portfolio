1695e431b2187d270399ea34ccc165a0ef5e56f3
Jarles Sayhare <jarlesjrlz@gmail.com>
2022-12-01 00:55:07 -0300

    adição do scroll reveal no site

--- scripts.js
+++ scripts.js
@@ -85,3 +85,16 @@ function loop() {
 
 loop()
 
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.card-projeto', { duration: 2500 });
+sr.reveal('.paragrafo', { duration: 2000 });
+sr.reveal('.bloco-img', { duration: 2000 });
+sr.reveal('.botao-sobremim', { duration: 2000 });
+sr.reveal('.container-skills', { duration: 3000 });
+sr.reveal('.um', { duration: 1500 });
+sr.reveal('.dois', { duration: 3500 });
+sr.reveal('.tres', { duration: 5000 });
+sr.reveal('.quatro', { duration: 6000 });
+
