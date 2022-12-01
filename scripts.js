5a86ddd9214b70bb15ec2cbcaa46923dc236f5be
Jarles Sayhare <jarlesjrlz@gmail.com>
2022-12-01 20:51:20 -0300

    melhorias em funcionalidades do site, modificação na paleta de cores, e aba sobre mim refeita

--- scripts.js
+++ scripts.js
@@ -86,9 +86,10 @@ function loop() {
 loop()
 
 
+
+
 window.sr = ScrollReveal({ reset: true });
 
-sr.reveal('.card-projeto', { duration: 2500 });
 sr.reveal('.paragrafo', { duration: 2000 });
 sr.reveal('.bloco-img', { duration: 2000 });
 sr.reveal('.botao-sobremim', { duration: 2000 });
@@ -97,4 +98,6 @@ sr.reveal('.um', { duration: 1500 });
 sr.reveal('.dois', { duration: 3500 });
 sr.reveal('.tres', { duration: 5000 });
 sr.reveal('.quatro', { duration: 6000 });
+
+
 
