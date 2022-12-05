04a5d52e04d32d632e36d24b77fd689d41836167
Jarles Sayhare <jarlesjrlz@gmail.com>
2022-12-05 10:48:05 -0300

    melhoria no scroll reveal

--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -1,9 +1,5 @@
 window.sr = ScrollReveal({ reset: true });
 
-sr.reveal('.paragrafo', { duration: 2000 });
-sr.reveal('.bloco-img', { duration: 2000 });
-sr.reveal('.botao-sobremim', { duration: 2000 });
-sr.reveal('.container-skills', { duration: 3000 });
 sr.reveal('.um', { duration: 1500 });
 sr.reveal('.dois', { duration: 3500 });
 sr.reveal('.tres', { duration: 5000 });
